2ef3494056e478bde152e0acff736170c0f6d088
Danilo Amaral <danilo123amaral@gmail.com>
2023-09-20 20:39:21 -0300

    :sparkles: feat: Adicionando certificados ao carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -11,12 +11,20 @@ export function Carousel() {
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="7" aria-label="Slide 8"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="8" aria-label="Slide 9"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="9" aria-label="Slide 10"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="10" aria-label="Slide 11"></button>
                     </div>
                     {/* As legendas foram desativadas até que sejam necessárias  */}
                     <div className="carousel-inner">  
                         <div className="carousel-item img-fluid  active">
                             <Link href="#">
-                                <Image src="/img/nasa.jpg" className="d-block w-100" alt="..." width={1950} height={800} />
+                                <Image src="/img/inteligencia-02.jpg" className="d-block w-100" alt="..." width={1950} height={800} />
                             </Link>
                             {/* <div className="carousel-caption d-none  d-md-block">
                                 <h5>titulo</h5>
@@ -25,7 +33,7 @@ export function Carousel() {
                         </div>
                         <div className="carousel-item">
                             <Link href="#">
-                                <Image src="/img/tec03.jpg" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
+                                <Image src="/img/certificado-react-coder.PNG" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
                             </Link>
                             {/* <div className="carousel-caption  d-none  d-md-block">
                                 <h5>titulo</h5>
@@ -34,7 +42,79 @@ export function Carousel() {
                         </div>
                         <div className="carousel-item">
                             <Link href="#">
-                                <Image src="/img/image-carousel03.jpg" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
+                                <Image src="/img/bootcamp-data-science.PNG" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
+                            </Link>
+                            {/* <div className="carousel-caption d-none d-md-block">
+                                <h5>titulo</h5>
+                                <p>Descrição</p>
+                            </div> */}
+                        </div>
+                        <div className="carousel-item">
+                            <Link href="#">
+                                <Image src="/img/certificado-web-moderno.jpg" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
+                            </Link>
+                            {/* <div className="carousel-caption d-none d-md-block">
+                                <h5>titulo</h5>
+                                <p>Descrição</p>
+                            </div> */}
+                        </div>
+                        <div className="carousel-item">
+                            <Link href="#">
+                                <Image src="/img/typescript.PNG" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
+                            </Link>
+                            {/* <div className="carousel-caption d-none d-md-block">
+                                <h5>titulo</h5>
+                                <p>Descrição</p>
+                            </div> */}
+                        </div>
+                        <div className="carousel-item">
+                            <Link href="#">
+                                <Image src="/img/angular-bootcamp.PNG" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
+                            </Link>
+                            {/* <div className="carousel-caption d-none d-md-block">
+                                <h5>titulo</h5>
+                                <p>Descrição</p>
+                            </div> */}
+                        </div>
+                        <div className="carousel-item">
+                            <Link href="#">
+                                <Image src="/img/machine-learning01.PNG" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
+                            </Link>
+                            {/* <div className="carousel-caption d-none d-md-block">
+                                <h5>titulo</h5>
+                                <p>Descrição</p>
+                            </div> */}
+                        </div>
+                        <div className="carousel-item">
+                            <Link href="#">
+                                <Image src="/img/redes-neurais.PNG" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
+                            </Link>
+                            {/* <div className="carousel-caption d-none d-md-block">
+                                <h5>titulo</h5>
+                                <p>Descrição</p>
+                            </div> */}
+                        </div>
+                        <div className="carousel-item">
+                            <Link href="#">
+                                <Image src="/img/etl-python.PNG" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
+                            </Link>
+                            {/* <div className="carousel-caption d-none d-md-block">
+                                <h5>titulo</h5>
+                                <p>Descrição</p>
+                            </div> */}
+                        </div>
+                        <div className="carousel-item">
+                            <Link href="#">
+                                <Image src="/img/modelagem-de-dados-sql.PNG" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
+                            </Link>
+                            {/* <div className="carousel-caption d-none d-md-block">
+                                <h5>titulo</h5>
+                                <p>Descrição</p>
+                            </div> */}
+                        </div>
+                        <div className="carousel-item">
+                            <Link href="#">
+                                <Image src="/img/responsividade.PNG" className="img-fluid d-block w-100" alt="..." width={1950} height={800} />
                             </Link>
                             {/* <div className="carousel-caption d-none d-md-block">
                                 <h5>titulo</h5>
