f7c01c56e2c2313800cc000c720598d429af08f5
Danilo Amaral <danilo123amaral@gmail.com>
2023-09-20 12:25:08 -0300

    :sparkles: feat: Página front end

--- src/pages/front.tsx
+++ src/pages/front.tsx
@@ -14,7 +14,7 @@ export default function front() {
                 <section className="container-title">
                     <div className="image">
                         <Image
-                            src="/img/portfolio01.jpg"
+                            src="/img/front-end01.jpg"
                             alt="capa"
                             layout="fill"
                             objectFit="fill"
@@ -26,28 +26,58 @@ export default function front() {
                 <section className="container-cards">
                     <figure className="card">
                         <Card
-                            titulo="Template Administrativo"
+                            titulo="Meu site (Navegue pelo código fonte desse site e veja como eu o desenvolvi)"
+                            image="/img/site-image.jpg"
+                            link="https://github.com/Danilo55Amaral/Danilo-Amaral-Site"
+                        />
+                    </figure>
+                    <figure className="card">
+                        <Card
+                            titulo="DT Money 2.0 (Nova versão do Sistema de controle de finanças com novas funcionalidades e um visual mais clean)"
+                            image="/img/finance-02.png"
+                            link="https://github.com/Danilo55Amaral/dtmoney2.0"
+                        />
+                    </figure>
+                    <figure className="card">
+                        <Card
+                            titulo="Versionamento de Código com Git e GitHub"
+                            image="/img/tec02.jpg"
+                            link="https://github.com/Danilo55Amaral/Git-GitHub"
+                        />
+                    </figure>
+                    <figure className="card">
+                        <Card
+                            titulo="Visite meu GitHub"
+                            image="/img/portfolio02.jpg"
+                            link="https://github.com/Danilo55Amaral"
+                        />
+                    </figure>
+                </section>
+                <section className="container-cards">
+                    <figure className="card">
+                        <Card
+                            titulo="Template Administrativo (Sistema administrativo com Login utilizando o API firebase do Google)"
                             image="/img/template-admin01.PNG"
                             link="https://github.com/Danilo55Amaral/Projeto-Template-Administrativo"
                         />
                     </figure>
                     <figure className="card">
                         <Card
-                            titulo="Ignite Timer"
+                            titulo="Ignite Timer (Projeto de Temporizador)"
                             image="/img/ignite-timer.PNG"
                             link="https://github.com/Danilo55Amaral/Ignite-Timer"
                         />
                     </figure>
                     <figure className="card">
                         <Card
-                            titulo="Ignite Feed"
+                            titulo="Ignite Feed (Projeto que simula um feed de rede social)"
                             image="/img/ignite-feed.png"
                             link="https://github.com/Danilo55Amaral/Ignite-Feed"
                         />
                     </figure>
                     <figure className="card">
                         <Card
-                            titulo="Dt Money"
+                            titulo="Dt Money (Sistema de Controle de Finanças)"
                             image="/img/dt-money-01.png"
                             link="https://github.com/Danilo55Amaral/dtmoney"
                         />
@@ -56,14 +86,14 @@ export default function front() {
                 <section className="container-cards">
                     <figure className="card">
                         <Card
-                            titulo="Quiz Projeto FullStack"
+                            titulo="Quiz Projeto FullStack (Quiz de Perguntas e respostas)"
                             image="/img/quiz-full-01.PNG"
                             link="https://github.com/Danilo55Amaral/Projeto-Quiz-FullStack"
                         />
                     </figure>
                     <figure className="card">
                         <Card
-                            titulo="Monty Hall"
+                            titulo="Monty Hall (Jogo da porta misteriosa, descubra em qual porta está o presente)"
                             image="/img/montyHall03.jpg"
                             link="https://github.com/Danilo55Amaral/Desafio-Monty-Hall"
                         />
@@ -77,7 +107,7 @@ export default function front() {
                     </figure>
                     <figure className="card">
                         <Card
-                            titulo="Calculadora"
+                            titulo="Calculadora (A Calculadora mais linda já criada ^-^)"
                             image="/img/calculadora-react.PNG"
                             link="https://github.com/Danilo55Amaral/Desafio-Calculadora-React"
                         />
